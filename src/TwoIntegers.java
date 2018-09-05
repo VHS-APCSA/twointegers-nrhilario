74d67f903649103470076c9a5daf8431a320a105
bob <bob@APCSA>
2018-09-05 14:52:32 -0700

    made a TwoIntegers class with two instance variables

---
+++ src/TwoIntegers.java
@@ -0,0 +1,39 @@
+
+public class TwoIntegers 
+{
+	private int first;
+	private int second;
+	
+	public TwoIntegers()
+	{
+		first = -1;
+		second = -1;
+	}
+	public TwoIntegers(int first, int second)
+	{
+		this.first = first;
+		this.second = second;
+	}
+	
+	public int getFirst()
+	{
+		return first;
+	}
+	public void setFirst(int first)
+	{
+		this.first = first;
+	}
+	
+	public int getSecond()
+	{
+		return second;
+	}
+	public void setSecond(int second)
+	{
+		this.second = second;
+	}
+	public TwoIntegers Arithmetic()
+	{
+		
+	}
+}
